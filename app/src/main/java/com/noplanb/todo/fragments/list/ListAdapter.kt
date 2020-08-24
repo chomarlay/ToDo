ba7b9745990e82e47425c1b502cfeb16f85aba6b
chomarlay <chomarlay.aung@gmail.com>
2020-08-24 17:50:24 +1000

    Added recycler view adapter

---
+++ app/src/main/java/com/noplanb/todo/fragments/list/ListAdapter.kt
@@ -0,0 +1,43 @@
+package com.noplanb.todo.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.noplanb.todo.R
+import com.noplanb.todo.data.models.Priority
+import com.noplanb.todo.data.models.ToDoData
+import kotlinx.android.synthetic.main.row_layout.view.*
+
+class ListAdapter(): RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+    private var dataList = emptyList<ToDoData>()
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
+        return MyViewHolder(view);
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.itemView.title_txt.text = dataList[position].title
+        holder.itemView.description_txt.text = dataList[position].description
+        val priority = dataList[position].priority
+        when (priority){
+            Priority.HIGH -> holder.itemView.priority_indicator.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.red))
+            Priority.MEDIUM -> holder.itemView.priority_indicator.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.yellow))
+            Priority.LOW -> holder.itemView.priority_indicator.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
+        }
+    }
+
+    fun setData(toDoData : List<ToDoData>) {
+        this.dataList = toDoData
+        notifyDataSetChanged()
+    }
+
+}
--- app/src/main/java/com/noplanb/todo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/noplanb/todo/fragments/list/ListFragment.kt
@@ -3,11 +3,18 @@ package com.noplanb.todo.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.noplanb.todo.R
+import com.noplanb.todo.data.viewmodel.ToDoViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
+
+    private val adapter: ListAdapter by lazy { ListAdapter() }
+    private val toDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,12 +22,20 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_list, container, false)
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        toDoViewModel.getAllData.observe(viewLifecycleOwner, Observer{
+            data-> adapter.setData(data)
+        })
+
         view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
         view.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+
         // set menu
         setHasOptionsMenu(true)
         return view
